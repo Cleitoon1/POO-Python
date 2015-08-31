780a5b3d94a4de4fe80366dcccc68d92ba0bf797
Cleitoon1 <cleiton.alvesjc@gmail.com>
2015-08-30 22:18:01 -0300

    Minimos Quadrados
    Update

--- tarefa2.py
+++ tarefa2.py
@@ -16,20 +16,29 @@ assert [1, 4, 9] == quadrado_menores(9)
 assert [1, 4, 9] == quadrado_menores(11)
 
 
-def soma_quadrados(n):
-
-    if n > 0:
-        menores = quadrado_menores(n)
-        ultimo = menores[-1]
-        if ultimo == n:
-            return [n]
+def soma_quadrados (n):
+    if n>0:
+        menores=quadrados_menores(n)
+        ultimo=menores[-1]
+        if ultimo==n:
+           return [n]
         else:
-            lista=[ultimo]
-            n-=ultimo
-            lista.extend(soma_quadrados(n))
-            return lista
+           ultimo=menores.pop()
+           lista_escolhida=[ultimo]
+           lista_escolhida.extend((soma_quadrados(n-ultimo)))
+           
+           while menores:
+               lista_escolhida_2=gerar_solucao(menores, n)
+               if len (lista_escolhida_2)<len(lista_escolhida):
+                   lista_escolhida=lista_escolhida_2
+           return lista_escolhida                      
+    return [0]
 
-    return[0]
+def gerar_solucao(menores,n):
+    ultimo=menores.pop()
+    lista_escolhida=[ultimo]
+    lista_escolhida.extend(soma_quadrados(n-ultimo))
+    return lista_escolhida
 
 assert [0] == soma_quadrados(0)
 assert [1] == soma_quadrados(1)
