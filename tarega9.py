f50427d84cb5b83e8d84522eec64e2773c1afcac
Cleitoon1 <cleiton.alvesjc@gmail.com>
2015-10-30 23:09:41 -0200

    Anagramas
    Verificar Anagrama e realizar testes

--- tarega9.py
+++ tarega9.py
@@ -1,17 +1,19 @@
-from unittest.case import TestCase
-def anagrama(a):
-    if a == a[::-1]:
+from unittest import TestCase
+def anagrama(a, b):
+    if a.replace(" ", "") == b[::-1].replace(" ", ""):
         return True
     print(a)
     return False
 
 class SomaTest(TestCase):
     def test_anagrama(self):
-        self.assertTrue('e', anagrama('e'))
-        self.assertTrue('a', anagrama('a'))
-        self.assertTrue('a', anagrama('a'))
-        self.assertTrue('a', anagrama('a'))
-        self.assertTrue('ba', anagrama('ab'))
-        self.assertTrue('ab', anagrama('ba'))
-        self.assertTrue('a', anagrama('ba'))
-
+        self.assertTrue(anagrama('', ''))
+        self.assertTrue(anagrama(' ', ''))
+        self.assertTrue(anagrama('a', 'a '))
+        self.assertFalse(anagrama('b', 'a'))
+        self.assertTrue(anagrama('a'))
+        self.assertTrue(anagrama('b a','a b'))
+        self.assertTrue(anagrama('ab','ba'))
+        self.assertTrue(anagrama('casa', 'asac'))
+        self.assertFalse(anagrama('cleiton', 'cadeira'))
+        self.assertFalse(anagrama('python', 'java'))
