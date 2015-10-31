7ed7b81e3d56296edd3710a621739ddadd38cfab
Cleitoon1 <cleiton.alvesjc@gmail.com>
2015-10-30 23:23:56 -0200

    Anagramas - Finalizado
    Verificar se são anagramas  e testes unitarios finalizados.

--- tarega9.py
+++ tarega9.py
@@ -1,8 +1,7 @@
 from unittest import TestCase
 def anagrama(a, b):
-    if a.replace(" ", "") == b[::-1].replace(" ", ""):
+    if a.replace(" ", "").upper() == b[::-1].replace(" ", "").upper():
         return True
-    print(a)
     return False
 
 class SomaTest(TestCase):
@@ -11,7 +10,7 @@ class SomaTest(TestCase):
         self.assertTrue(anagrama(' ', ''))
         self.assertTrue(anagrama('a', 'a '))
         self.assertFalse(anagrama('b', 'a'))
-        self.assertTrue(anagrama('a'))
+        self.assertTrue(anagrama('a', ' a      '))
         self.assertTrue(anagrama('b a','a b'))
         self.assertTrue(anagrama('ab','ba'))
         self.assertTrue(anagrama('casa', 'asac'))
